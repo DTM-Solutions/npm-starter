6115f1ba03f2d6848bebb544113183d14cbcd7bf
milua25 <ademiluaayo94@gmail.com>
2024-03-01 02:13:59 -0500

    setup done

---
+++ src/utils/flagsmith.ts
@@ -0,0 +1,19 @@
+// import Flagsmith from "flagsmith-nodejs"; 
+
+// // const Flagsmith = require('flagsmith-nodejs');
+
+// const flagsmith = new Flagsmith({
+//  environmentKey: process.env.FLAGSMITH_KEY as string,
+//  requestTimeoutSeconds: 60
+// });
+
+// export default flagsmith
+
+import Flagsmith from "flagsmith-nodejs";
+
+const flagsmith = new Flagsmith({
+  environmentKey: process.env.FLAGSMITH_KEY as string,
+  requestTimeoutSeconds: 60,
+});
+
+export default flagsmith;
